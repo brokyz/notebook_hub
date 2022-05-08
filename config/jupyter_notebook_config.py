599fae96546c975fa5c931b3dcc8984b342eab4c
13roky <boqiang6450@outlook.com>
2022-05-08 23:32:24 +0800

    fix bugsï¼âââ

--- config/jupyter_notebook_config.py
+++ config/jupyter_notebook_config.py
@@ -2,7 +2,7 @@
 
 c.NotebookApp.ip = "*"
 c.NotebookApp.open_browser = False
-c.NotebookApp.notebook_dir = "/home/${NB_USER}/projects"
+c.NotebookApp.notebook_dir = /home/$NB_USER/projects
 c.NotebookApp.port = 8888
 c.NotebookApp.terminado_settings = {'shell_command' : ['/bin/bash']}
 
